a37de77cef46c8a2adfea346557917c1b0fdd903
borghiste <158812327+borghiste@users.noreply.github.com>
2025-03-11 16:43:28 +0100

    complete

--- src/app/Quiz/components/Options.tsx
+++ src/app/Quiz/components/Options.tsx
@@ -1,8 +1,29 @@
-export default function Options(){
+import { useState } from "react"
+
+export default function Options({options, setcheckedAnswer, checkSolution, solution}){
+
+ const [options2, setOptions] = useState(options)
+
+    const test = options2?.map((option)=>(
+      <li className="bg-[#80d2f2] rounded-full flex 
+      items-center my-2  text-[#351826] w-96 right-4 relative" 
+      key={option}>
+<input type="radio" id={option} 
+name={options} 
+onChange={(e)=>{setcheckedAnswer}} 
+value={option}
+className=' size-4' 
+
+onClick={(e)=>{checkSolution(e.target.value, solution)}}/>{option}</li>      
+    ))
     return(
-        <ul>
-            <li>option1</li>
-           
+        <ul className=" z-20 absolute  pt-[1rem] text-left">
+
+            
+        {test}
+            
+
+        
 
         </ul>
     )
