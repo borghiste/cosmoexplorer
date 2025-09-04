b3fe85b21eecec97ad93071dfee2e5b9f4b09ec5
borghiste <158812327+borghiste@users.noreply.github.com>
2025-09-04 13:47:17 +0200

    fix(planets): fix title

--- src/app/Planets/page.tsx
+++ src/app/Planets/page.tsx
@@ -196,7 +196,7 @@ export default function Planets() {
   
     <div style={{ height: '100vh', width: '100%', overflowY: 'auto', overflow:'hidden', position:'relative' }}>
 
-        <h2 className='lg:text-6xl text-5xl mx-40'>Planets</h2>
+        <h2 className='lg:text-6xl text-5xl text-center'>Planets</h2>
         <p className='w-max-96 text-center'>The Solar System is composed of eight main planets that orbit around the Sun, our central star. These planets are categorized into two groups: the four inner rocky planets (Mercury, Venus, Earth, and Mars) and the four outer gas or ice giants (Jupiter, Saturn, Uranus, and Neptune). Each planet has its own unique characteristics, atmosphere, and environment.</p>
 
         <p className='bg-[var(--violet)] rounded-r-2 max-w-[9rem] text-sm text-center text-black m-auto self-center'>Click on a planet to see its description</p>
